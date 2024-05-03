ed9870416d58cfc26ad3788ae018b42f65948183
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2024-05-03 08:57:40 +0200

    Create app

---
+++ src/test/java/pl/com/itsystems/springcontactform/SpringContactFormApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.itsystems.springcontactform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContactFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
